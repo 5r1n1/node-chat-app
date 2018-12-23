fcc89bd7f7ed53469bff0a0f22f2ad1e86dfc086
Srinivas Gopalakrishnan <srinig@msn.com>
2018-12-23 10:04:13 +0530

    Add msg form and list incoming msgs in browser

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,8 +1,13 @@
-/* eslint-disable no-console */
-/* global io */
-
 var socket = io ()
 
 socket.on ('connect', function () {console.log ('Connected to Server')})
 socket.on ('disconnect', function () {console.log ('Disconnected to Server')})
-socket.on ('newMessage', function (msg) {console.log ('New Message: ', msg)})
+socket.on ('newMessage', function (msg) {
+    $ ('#messages').append ($ ('<li>,/li>').text (`${msg.from}: ${msg.text}`))
+})
+
+$ ('#message-form').on ('submit', function (e) {
+    e.preventDefault()
+    socket.emit ('createMessage', {from: 'Srini', text: $ ('[name=message]').val()}
+    )    
+})
--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
-/* eslint-disable no-console */
 const path     = require ('path')
 const http     = require ('http')
 const express  = require ('express')
 const socketIO = require ('socket.io')
 
-const genMsg   = require ('./utils/message')
+const {genMsg}   = require ('./utils/message')
 
 const port = process.env.PORT || 3001
 const publicPath = path.join (__dirname, '../public/')
@@ -16,9 +15,8 @@ const io = socketIO (server)
 io.on ('connect', socket => {
     socket.emit ('newMessage', genMsg ('Admin','Welcome to the chat room'))
     socket.broadcast.emit ('newMessage', genMsg ('Admin', 'A new user has entered the chat room'))
-    socket.on ('createMessage', msg => io.emit ('newMessage', genMsg (msg.from, msg.text)))        
+    socket.on ('createMessage', msg => io.emit ('newMessage', genMsg (msg.from, msg.text)))
 })
 
 app.use (express.static(publicPath))
-
 server.listen (port, () => console.log (`Chat app is listening on port ${port}!`))
